61130311c875f0601d2c8863d8e51cecb2e510ac
Hoàng Ng. <hoangnguyen1247@gmail.com>
2025-12-09 04:46:46 +0000

    fix: update script install neovim

--- nvim/install-nvim.sh
+++ nvim/install-nvim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # sudo apt install neovim
-sudo add-apt-repository ppa:neovim-ppa/stable
+# sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 sudo apt install neovim
