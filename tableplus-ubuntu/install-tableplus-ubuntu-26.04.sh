6ee84c916ead8f15917dfd41a18e2d5aac9f5ff2
Hoàng Ng. <hoangnguyen1247@gmail.com>
2026-05-25 17:16:23 +0700

    feat: add script install tableplus for ubuntu-26

---
+++ tableplus-ubuntu/install-tableplus-ubuntu-26.04.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+wget -qO - https://deb.tableplus.com/apt.tableplus.com.gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/tableplus-archive.gpg > /dev/null
+sudo add-apt-repository "deb [arch=amd64] https://deb.tableplus.com/debian/26 tableplus main"
+sudo apt update
+sudo apt install tableplus
