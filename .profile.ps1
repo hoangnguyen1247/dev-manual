ba8b2de3b7e173ee7ddd21d7f923fe213c7c72ab
Hoàng Ng. <hoangnguyen1247@gmail.com>
2026-08-18 09:00:41 +0700

    feat: add git alias

--- .profile.ps1
+++ .profile.ps1
@@ -17,3 +17,15 @@ Set-Alias -Name gdev -Value GoToCDev
 
 function CodeCDev { code "$WorkspaceDir\dev-infra\dev-manual" }
 Set-Alias -Name cdev -Value CodeCDev
+
+function GitPullMaster { git pull origin master }
+Set-Alias -Name gitm -Value GitPullMaster
+
+function GitPushMaster { git push origin master }
+Set-Alias -Name gitpm -Value GitPushMaster
+
+function GitPullCurrent { git pull origin $(git branch --show-current) }
+Set-Alias -Name gitc -Value GitPullCurrent
+
+function GitPushCurrent { git push origin $(git branch --show-current) }
+Set-Alias -Name gitpc -Value GitPushCurrent
