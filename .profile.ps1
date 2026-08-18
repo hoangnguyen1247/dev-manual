078f039cb4c8939bcf6908facbc83003cf28fa88
Hoàng Ng. <hoangnguyen1247@gmail.com>
2026-08-18 08:55:53 +0700

    feat: set alias with params

--- .profile.ps1
+++ .profile.ps1
@@ -5,10 +5,15 @@
 #     . $profileEnvScript
 # }
 
-
 Set-Alias -Name which -Value where.exe
+
+function SourceProfile { . $PROFILE }
+Set-Alias -Name sob -Value SourceProfile
 
 $WorkspaceDir = "C:\Users\hoang\workspace"
 
 function GoToCDev { Set-Location "$WorkspaceDir\dev-infra\dev-manual" }
-Set-Alias -Name cdev -Value GoToCDev
+Set-Alias -Name gdev -Value GoToCDev
+
+function CodeCDev { code "$WorkspaceDir\dev-infra\dev-manual" }
+Set-Alias -Name cdev -Value CodeCDev
